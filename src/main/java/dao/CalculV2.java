72b09349749eaa8af734fe11cdb67e1b5cb84670
SaraZaimSassi <sarahzaimsassi@gmail.com>
2024-10-21 14:51:35 +0100

    first commit

---
+++ src/main/java/dao/CalculV2.java
@@ -0,0 +1,13 @@
+package dao;
+
+import org.springframework.stereotype.Component;
+
+
+import org.springframework.stereotype.Component;
+@Component("cal2")
+public class CalculV2 implements ICalcul{
+@Override
+public double getValue() {
+    double d=150;
+    return d;
+}}
